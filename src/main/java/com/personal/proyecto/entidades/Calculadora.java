7018e25225c0bcce2e5e359a01b5cd5f7cde9f46
Carlos Lemus <lemusc956@gmail.com>
2019-09-20 16:05:45 -0600

    Metodo de resta de 2 numeros

--- src/main/java/com/personal/proyecto/entidades/Calculadora.java
+++ src/main/java/com/personal/proyecto/entidades/Calculadora.java
@@ -3,7 +3,12 @@ package com.personal.proyecto.entidades;
 public class Calculadora {
 	
 	public static int sumar(int a, int b) {
-		return a+b;
+		int res = a+b;
+		
+		return res;
 	}
-
+	
+	public static int restar(int a, int b) {
+		return a-b;
+	}
 }

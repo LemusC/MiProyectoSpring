b9810cc2cd016fa3ac6f829591eea27e1d15b640
Carlos Lemus <lemusc956@gmail.com>
2019-09-20 15:56:40 -0600

    Metodo de suma de 2 numeros

---
+++ src/main/java/com/personal/proyecto/entidades/Calculadora.java
@@ -0,0 +1,9 @@
+package com.personal.proyecto.entidades;
+
+public class Calculadora {
+	
+	public static int sumar(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ src/main/java/com/personal/proyecto/entidades/Testing.java
@@ -0,0 +1,5 @@
+package com.personal.proyecto.entidades;
+
+public class Testing {
+
+}
